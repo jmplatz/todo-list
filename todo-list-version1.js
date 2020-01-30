54cb943d01cd9b915a64d8e5bed61d3ad7e031a0
Jordan <jordan.platz1@gmail.com>
2020-01-29 19:17:30 -0800

    addTodo create obj, added toggleCompleted function

--- todo-list-version1.js
+++ todo-list-version1.js
@@ -8,16 +8,30 @@ var todoList = {
   },
 
   // It should have a funciton to add todos
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false,
+    });
+    this.displayTodos();
   },
   // It should have a function to change todos
-  changeTodo: function(position, change) {
-    this.todos[position] = change;
+  changeTodo: function(position, newText) {
+    var todo = this.todos[position];
+    todo.todoText = newText;
+    this.displayTodos();
   },
 
   // It should have a function to delete todos
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+
+  // It should have a function to toggle if todo has been completed or not
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
   },
 }

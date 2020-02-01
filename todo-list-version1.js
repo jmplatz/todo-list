ca51b1182289e00cb76b2c555feef2ed05b77fff
Jordan <jordan.platz1@gmail.com>
2020-02-01 10:46:00 -0800

    displayTodos: now shows todoText prop and if completed

--- todo-list-version1.js
+++ todo-list-version1.js
@@ -1,37 +1,46 @@
 var todoList = {
-
   todos: [],
-
   // It should have a function to display todos
-  displayTodos: function() {
-    console.log("My todo's: " + this.todos);
+  displayTodos: function () {
+    if (this.todos.length === 0) {
+      console.log("List is empty");
+    } else {
+      console.log("My todo's:");
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true)
+          console.log("( X ) " + this.todos[i].todoText);
+        else
+          console.log("(   ) " + this.todos[i].todoText);
+      }
+    }
   },
 
   // It should have a funciton to add todos
-  addTodo: function(todoText) {
+  addTodo: function (todoText) {
     this.todos.push({
       todoText: todoText,
-      completed: false,
+      completed: false
     });
     this.displayTodos();
   },
+
   // It should have a function to change todos
-  changeTodo: function(position, newText) {
+  changeTodo: function (position, newText) {
     var todo = this.todos[position];
     todo.todoText = newText;
     this.displayTodos();
   },
 
   // It should have a function to delete todos
-  deleteTodo: function(position) {
+  deleteTodo: function (position) {
     this.todos.splice(position, 1);
     this.displayTodos();
   },
 
-  // It should have a function to toggle if todo has been completed or not
-  toggleCompleted: function(position) {
+  // It should have a function to toggle if a todo has been completed or not
+  toggleCompleted: function (position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
-  },
-}
+  }
+};
